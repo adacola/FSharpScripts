868a9bdd35efc344f0006d96817a399a131fa73c
adacola <adacola@le.to>
2012-07-04 12:42:43 +0900

    ソース追加、書きかけのものがあったので修正

---
+++ DataAlgorithm15.fsx
@@ -0,0 +1,31 @@
+﻿// 解を列挙する関数
+let solve xBlockNum yBlockNum =
+    let [xLen; yLen] = [xBlockNum; yBlockNum] |> List.map (fun n -> n * 2 - 1)
+    let markingNum = xBlockNum * yBlockNum * 2 - 1
+    let directions = [1, 0; 0, 1; -1, 0; 0, -1]
+    let memo = System.Collections.Generic.HashSet(HashIdentity.Structural)
+
+    let getNext markedSet candidates =
+        candidates |> Seq.map (fun ((x, y), (dx, dy)) ->
+            let nx, ny = x + dx, y + dy
+            let newMarked = markedSet |> Set.add (x, y) |> Set.add (nx, ny)
+            directions |> List.choose (fun (dx, dy) ->
+                if 1 <= nx + dx && nx + dx <= xLen && 1 <= ny + dy && ny + dy <= yLen
+                then Some((nx + dx, ny + dy), (dx, dy)) else None)
+            |> Seq.append candidates
+            |> Seq.filter (fun ((x, y), (dx, dy)) -> Set.contains (x + dx, y + dy) newMarked |> not)
+            |> fun nextCandidates -> newMarked, nextCandidates)
+
+    let rec solve markedSet candidates = seq {
+        if memo.Add markedSet then
+            if Set.count markedSet = markingNum then yield markedSet
+            else yield! getNext markedSet candidates |> Seq.collect ((<||) solve) }
+
+    solve (set [1, 1]) [(2, 1), (1, 0); (1, 2), (0, 1)]
+    |> Seq.map (fun result ->
+        [for y in 0 .. yLen + 1 ->
+            [for x in 0 .. xLen + 1 -> if Set.contains (x, y) result then "*" else "-"]])
+
+// 解を10個表示
+solve 4 4 |> Seq.take 10 |> Seq.iter (fun result ->
+    result |> Seq.iter (String.concat "" >> printfn "%s"); printfn "")
--- validateIsbn13.fsx
+++ validateIsbn13.fsx
@@ -7,16 +7,6 @@ let validateIsbn13 isbn =
     && List.exists ((=) isbn.[.. 2]) ["978"; "979"]
     && isbn.[.. 11] |> Seq.map (string >> int) |> modulous10weight31 |> (=) (int isbn.[12 .. 12])
 
-let completeIsbn13 isbn =
-    match String.length isbn with 13 -> Some isbn | _ -> None
-    |> Option.bind (Seq.mapi (fun i x -> (i, x)) >> Seq.filter (snd >> (=) '?') >> Seq.toList
-                    >> function [i, _] -> Some i | _ -> None)
-    |> Option.bind (fun brokenIndex ->
-        let digit = isbn.[.. 11] |> Seq.map ((function '?' -> "0" | x -> string x) >> int) |> modulous10weight31
-
-
-
-
 validateIsbn13 "9784101092058" |> (=) true |> printfn "%A"
 validateIsbn13 "9784062772211" |> (=) true |> printfn "%A"
 validateIsbn13 "9784150315684" |> (=) false |> printfn "%A"
