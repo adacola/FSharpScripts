25f5a06697c07b2cd111535e64d45cf243768ef4
adacola <adacola@le.to>
2012-08-06 04:00:02 +0900

    ソース追加

---
+++ exercise5_732.fsx
@@ -0,0 +1,12 @@
+﻿(*
+文字列が与えられたとき、この文字列を出力するBrainf*ckプログラムを生成せよ。
+*)
+let toBF (input : string) =
+    let diff x y =
+        let c, s = if x - y < 128uy then x - y, "-" else y - x, "+"
+        String.replicate (int c) s + "."
+    let bytes = System.Text.Encoding.UTF8.GetBytes input
+    let prevs = bytes |> Seq.append [0uy] |> Seq.pairwise |> Seq.map ((<||) diff)
+    let zeros = bytes |> Seq.map (diff 0uy >> (+) ">")
+    Seq.zip prevs zeros |> Seq.map (fun (prev, zero) -> [prev; zero] |> Seq.minBy String.length)
+    |> String.concat ""
